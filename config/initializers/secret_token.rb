14de32df63835f4bfe13b56c49170285bf97ab86
J2Alpha <johnjohnboeva@gmail.com>
2014-02-23 01:29:17 +0100

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WelcomeToAppstacy::Application.config.secret_key_base = '304c6317723f1b4e9d35893a4f26e96a11234042f614953c884b3fb648e9e11b9624661913fb2ccb4a52e2a53ef92223536d0a43d47db72e62727f334d497191'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WelcomeToAppstacy::Application.config.session_store :cookie_store, key: '_welcome_to_appstacy_session'
